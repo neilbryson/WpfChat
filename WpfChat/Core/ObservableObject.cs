5b23276a2c2cdfc50494363894f4265b5f4b17fb
Neil Bryson Cargamento <neilbryson@users.noreply.github.com>
2021-09-03 12:23:15 +0800

    final implementation

---
+++ WpfChat/Core/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfChat.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- WpfChat/MVVM/ViewModel/MainViewModel.cs
+++ WpfChat/MVVM/ViewModel/MainViewModel.cs
@@ -1,25 +1,66 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Windows.Automation;
+using WpfChat.Core;
 using WpfChat.MVVM.Model;
 
 namespace WpfChat.MVVM.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : ObservableObject
     {
         public ObservableCollection<Message> Messages { get; set; }
         public ObservableCollection<Contact> Contacts { get; set; }
+
+        public RelayCommand SendCommand { get; set; }
+
+        private Contact _selectedContact;
+
+        public Contact SelectedContact
+        {
+            get { return _selectedContact; }
+            set
+            {
+                _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _messageText;
+
+        public string MessageText
+        {
+            get { return _messageText; }
+            set
+            {
+                _messageText = value;
+                OnPropertyChanged();
+            }
+        }
 
         public MainViewModel()
         {
             Messages = new ObservableCollection<Message>();
             Contacts = new ObservableCollection<Contact>();
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new Message
+                {
+                    Content = MessageText,
+                    IsFirstMessage = false
+                });
+
+                MessageText = "";
+            });
+
+            var fromUser = Faker.Name.FullName();
+            const string fromUserColour = "#409AFF";
 
             for (var i = 0; i < 4; i++)
             {
                 Messages.Add(new Message
                 {
-                    Username = Faker.Name.FullName(),
-                    UsernameColour = "#409AFF",
+                    Username = fromUser,
+                    UsernameColour = fromUserColour,
                     ImageSource = "https://i.pinimg.com/474x/c4/7a/b1/c47ab18aa9246a6c16c7619b41a536fd.jpg",
                     Content = Faker.Company.CatchPhrase(),
                     Time = DateTime.Now,
